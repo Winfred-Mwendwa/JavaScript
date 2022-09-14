e14c5a55e88690ffc09d5ef024a36866d25e0106
winnie <mwendwawinfred8@gmail.com>
2022-09-14 10:03:58 +0300

    seventeenth commit

--- arrow func.js
+++ arrow func.js
@@ -1,12 +1,16 @@
-//arrow functions are used to simplify functions with no body but only a simple return value
-const myFunc = function() {
-    const myVar = "value";
-    return myVar;
-  }
-console.log(myFunc);
+//arrow functions are inline functons used to simplify anonymous functions 
+//anonymous functions are unique to a code block and used only once. they are passed to another function
+
+document.addEventListener('click', function name() {
+    console.log('click')
+})
 
 //can be simplified to
-const myFunc2 = () => "value";
+document.addEventListener('click', () => console.log('click'))
 
-console.log(myFunc2);
-
+//get rid of function keyword as it is assumed
+//parameters go to the left of the arrow
+//empty brackets if no parameters
+//no need for brackets for a single parameter
+//everything that needs to be returned goes to the right of the arrow
+//get rid of curly braces
