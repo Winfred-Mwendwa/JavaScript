e691977b4a582325dbb56bfbc336c1d3cd27d2e6
winnie <mwendwawinfred8@gmail.com>
2022-09-24 15:04:14 +0300

    25th  commit

--- oop.js
+++ oop.js
@@ -47,3 +47,230 @@ function House(numBedrooms) {
   let myHouse= new House(5);
   
   console.log(myHouse instanceof House);
+
+  function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  // Only change code below this line
+  for (let property in canary) {
+    if (canary.hasOwnProperty(property)) {
+      ownProps.push(property);
+    }
+  }
+
+  function Dog(name) {
+    this.name = name;//own property
+    
+  }
+  Dog.prototype.numLegs=4;//prototype property
+  
+  let beagle = new Dog("Snoopy");
+console.log(beagle);
+
+//diffrence between own properties and prototype properties
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle1 = new Dog("Snoopy");
+  //
+
+  let ownProps1 = [];
+  let prototypeProps = [];
+  
+  for (let property in beagle1) {
+    if (beagle1.hasOwnProperty(property)) {
+      ownProps1.push(property);
+    } else {
+      prototypeProps.push(property);
+    }
+  }
+
+//refractor prototype properties by creating an object for them
+function Dog(name) {
+    this.name = name;
+  }
+  constructor:Dog, //define the constructor so it is not overidden
+  Dog.prototype = {
+    numLegs: 2,
+    eat: function() {
+      console.log("nom nom nom");
+  },
+  describe: function() {
+      console.log("My name is " + this.name);
+    }
+  };
+
+//check prototype of an object using isPrototypeOf()
+function Dog(name) {
+    this.name = name;
+  }
+  
+  let beagle2 = new Dog("Snoopy");
+  
+  
+  Dog.prototype.isPrototypeOf(beagle2);
+
+//a prototype has its own prototype; the Object prototype
+function Dog(name) {
+    this.name = name;
+  }
+  
+  let beagle3 = new Dog("Snoopy");
+  
+  Dog.prototype.isPrototypeOf(beagle3);  // yields true
+  
+  Object.prototype.isPrototypeOf(Dog.prototype);
+
+//Don't Repeat Yourself(DRY) principle using inheritance
+function Cat(name) {
+    this.name = name;
+  }
+  
+  Cat.prototype = {
+    constructor: Cat,
+    
+  };
+  
+  function Bear(name) {
+    this.name = name;
+  }
+  
+  Bear.prototype = {
+    constructor: Bear,
+    
+  };
+  
+  function Animal() { } //supertype containing properties common to all animals
+  
+  Animal.prototype = {
+    constructor: Animal,
+    eat: function() {
+      console.log("nom nom nom");
+    }
+  
+  };
+
+//inherit properties from the supertype with object.create(obj)
+//ChildObject.prototype = Object.create(ParentObject.prototype);
+let duck = Object.create(Animal.prototype); 
+let beagle4= Object.create(Animal.prototype);
+
+//set the child's prototype to an instance of the parent
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle5 = new Dog();
+
+//reset an inherited constructor property
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck1= new Bird();
+let beagle6 = new Dog();
+
+//add methods after inheritance
+//ChildObject.prototype.methodName = function() {...};
+function Animal() { }
+Animal.prototype.eat = function() { console.log("nom nom nom"); };
+
+function Dog() { }
+
+Dog.prototype=Object.create(Animal.prototype);
+Dog.prototype.constructor=Dog;
+Dog.prototype.bark= function () {
+  console.log('Woof!');
+}
+
+let beagle7 = new Dog();
+
+//overide inherited methods
+function Bird() { }
+
+Bird.prototype.fly = function() { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+Penguin.prototype.fly = function() { return "Alas, this is a flightless bird."; }//override
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+//Use a Mixin to Add Common Behavior Between Unrelated Objects
+//A mixin allows other objects to use a collection of functions.
+let bird = {
+    name: "Donald",
+    numLegs: 2
+  };
+  
+  let boat = {
+    name: "Warrior",
+    type: "race-boat"
+  };
+  
+  let glideMixin= function (obj) {
+    obj.glide= function() {
+      console.log('gliding');
+    }
+  };
+  glideMixin(bird);
+  glideMixin(boat);
+
+  //Use Closure to Protect Properties Within an Object from Being Modified Externally
+  //In JavaScript, a function always has access to the context in which it was created. This is called closure
+  function Bird() {
+    this.getWeight = function () { //getWeight  is a privileged method, because it has access to the private variable weight
+      let weight= 15;
+      return weight;
+  
+    }
+
+  }
+
+//the Immediately Invoked Function Expression (IIFE)
+//two parentheses () at the end of the function expression;after wrapping it in parenthesis cause it to be immediately executed or invoked.
+(function () {
+    console.log("A cozy nest is ready");
+  })();
+
+//an IIFE is often used to group related functionality into a single object or module.
+//the module here should return an object
+let funModule = (function () {
+    return {isCuteMixin: function(obj) {
+    obj.isCute = function() {
+      return true;
+    };
+  },
+  singMixin : function(obj) {
+    obj.sing = function() {
+      console.log("Singing to an awesome tune");
+    };
+  }
+    }
+  })();
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,26 +1,10 @@
-let users1 = {
-    Alan: {
-      age: 27,
-      online: false
-    },
-    Jeff: {
-      age: 32,
-      online: true
-    },
-    Sarah: {
-      age: 48,
-      online: false
-    },
-    Ryan: {
-      age: 19,
-      online: true
-    }
-  };
-  
-  function getArrayOfUsers(obj) { 
-    
-  return Object.keys(obj);
-    
+function Dog(name) {
+    this.name = name;
+    Dog.prototype.numLegs=4;
   }
   
-  console.log(getArrayOfUsers(users1));
+  
+  
+  // Only change code above this line
+  let beagle = new Dog("Snoopy");
+console.log(beagle);
